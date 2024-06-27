cbf02bc8d7a8311755d897a8c563a056bb0870a8
Alejandro Martinez <janusreal@gmail.com>
2024-06-27 11:21:51 -0400

    Agregado services.py (desafio guiado)

--- dia3/services.py
+++ dia3/services.py
@@ -1,5 +1,4 @@
 from dia3.models import Tarea, SubTarea
-from django.db import query1
 
 def recuperar_tareas_y_subtareas():
     # recuperar
@@ -9,34 +8,38 @@ def recuperar_tareas_y_subtareas():
 def crear_tarea(descripcion):
     t = Tarea(descripcion=descripcion)
     t.save()
-    imprimir_en_pantalla()
+    imprimir_en_pantalla() 
     #funciona
     
 def crear_subtarea(descripcion, idtarea):
     t = Tarea.objects.get(id=idtarea)
     st = SubTarea(descripcion=descripcion, tarea=t)
     st.save()
-    #funciona +-
+    imprimir_en_pantalla() 
+    #funciona
 
 def eliminar_tarea(idtarea):
     t = Tarea.objects.get(id=idtarea)
     t.eliminada = True  
     t.save()
+    imprimir_en_pantalla() 
     #funciona
 
 def eliminar_subtarea(idsubtarea):
     st = SubTarea.objects.get(id=idsubtarea)
     st.eliminada = True
     st.save()
+    imprimir_en_pantalla() 
     #funciona
 
 def imprimir_en_pantalla():
     tareas = recuperar_tareas_y_subtareas()
-    
+
     for t in tareas:
         print(f'[{t.id}] {t.descripcion}')
         for sub_tarea in t.subtareas.filter(eliminada=False):
             print(f'... [{sub_tarea.id}] {sub_tarea.descripcion}')
+    imprimir_en_pantalla() 
+    #funciona
     
-    #funciona        
-              
+             
